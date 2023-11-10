bf1582cc5564bce80a6d6835f8f1dde787b2e403
Robson A. Rocha <robson_it@outlook.com>
2023-11-10 10:18:29 -0300

    Configuração das rotas de Temas e alertas do tostify

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../pages/home/Home';
 import Login from '../pages/login/Login';
 import Cadastro from '../pages/cadastro/Cadastro';
+import ListaTemas from './Components/Temas/ListaTemas/ListaTemas';
+import FormularioTema from './Components/Temas/FormularioTema/FormularioTema';
+import DeletarTema from './Components/Temas/DeletarTema/DeletarTema';
 import Navbar from '../components/navbar/Navbar';
 import Footer from '../components/footer/Footer';
 import { AuthProvider } from '../contexts/AuthContext';
@@ -10,16 +13,20 @@ function MainRoute() {
   return (
     <AuthProvider>
       <BrowserRouter>
-        <Navbar/>
+        <Navbar />
         <div className='min-h-[80vh]'>
-            <Routes>
-              <Route path="/" element={<Login/>} />
-              <Route path="/login" element={<Login/>} />
-              <Route path="/home" element={<Home/>} />
-              <Route path="/cadastro" element={<Cadastro/>} />
-            </Routes>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/temas" element={<ListaTemas />} />
+            <Route path="/cadastroTema" element={<FormularioTema />} />
+            <Route path="/editarTema/:id" element={<FormularioTema />} />
+            <Route path="/deletarTema/:id" element={<FormularioTema />} />
+          </Routes>
         </div>
-        <Footer/>
+        <Footer />
       </BrowserRouter>
     </AuthProvider>
   )
